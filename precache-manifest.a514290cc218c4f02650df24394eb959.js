d8500a7edd9bb46912da90afb418acbd91c660b8
ffcabbar <ffcabbar@gmail.com>
2020-03-04 18:47:48 +0300

    Updates

---
+++ precache-manifest.a514290cc218c4f02650df24394eb959.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67c1c43231ebcd98ef0e230c1ff8ba9",
+    "url": "/react-apollo-client-example/index.html"
+  },
+  {
+    "revision": "412d35374f6ae86d8b30",
+    "url": "/react-apollo-client-example/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a828fb218feec3de0f54",
+    "url": "/react-apollo-client-example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "412d35374f6ae86d8b30",
+    "url": "/react-apollo-client-example/static/js/2.3756bcfb.chunk.js"
+  },
+  {
+    "revision": "5298b0565bb315b9314a8baf33940c9d",
+    "url": "/react-apollo-client-example/static/js/2.3756bcfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a828fb218feec3de0f54",
+    "url": "/react-apollo-client-example/static/js/main.c9f4985d.chunk.js"
+  },
+  {
+    "revision": "5afda357b0fb3412084b",
+    "url": "/react-apollo-client-example/static/js/runtime-main.72ac4baf.js"
+  },
+  {
+    "revision": "edfe1d162b4196d9197ae2225d4aa243",
+    "url": "/react-apollo-client-example/static/media/rickandmorty.edfe1d16.png"
+  }
+]);
